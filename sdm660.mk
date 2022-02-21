1fedcb80054383fbbe8320174f5c20c4fbb7a0d4
AT WORLD <atharvsutar3102003@gmail.com>
2022-02-21 23:24:57 +0530

    sdm660-common: Blissify

--- sdm660.mk
+++ sdm660.mk
@@ -261,7 +261,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-superior
+    $(LOCAL_PATH)/overlay-bliss
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
